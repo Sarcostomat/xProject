9ea5c8c21a8650237f0cc722e46f89f27b596432
Programmierer <Programmierer@Toshiba>
2015-08-15 19:43:32 +0200

    neueDaten

--- connecter/connect.php
+++ connecter/connect.php
@@ -3,4 +3,8 @@
 mysql_connect("localhost", "root", "") or die ("keine Verbindung zum Server");
 mysql_select_db("webusers") or die ("keine Verbindung zur Datenbank"); 
 
+
+
+
+echo 'asdsadsad';
 ?>
